b149dd7f54f76efb91761382919e7632a9a4732c
Alvin Chen <terrier150@gmail.com>
2016-09-29 23:49:27 -0700

    updated current.sql: NO created_at or updated_at

--- sql/current.sql
+++ sql/current.sql
@@ -28,8 +28,6 @@ CREATE TABLE IF NOT EXISTS `happyhopdb`.`locations` (
   `city` VARCHAR(255) NOT NULL,
   `state` VARCHAR(45) NOT NULL,
   `zip_code` INT(11) NOT NULL,
-  `created_at` DATETIME NULL DEFAULT NOW(),
-  `updated_at` DATETIME NULL DEFAULT NOW(),
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
 AUTO_INCREMENT = 64
@@ -43,8 +41,6 @@ CREATE TABLE IF NOT EXISTS `happyhopdb`.`hhprofiles` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   `description` VARCHAR(255) NOT NULL,
-  `created_at` DATETIME NOT NULL DEFAULT NOW(),
-  `updated_at` DATETIME NOT NULL DEFAULT NOW(),
   `locations_id` INT(11) NOT NULL,
   PRIMARY KEY (`id`, `locations_id`),
   INDEX `fk_hhprofiles_locations1_idx` (`locations_id` ASC),
